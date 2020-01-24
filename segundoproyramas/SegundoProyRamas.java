a89ddcfae4e0f5574f7454ed4631fd0b74102808
Miguelmc5 <mmartinezcosta@danielcastelao.org>
2020-01-24 13:54:09 +0100

    A1

---
+++ segundoproyramas/SegundoProyRamas.java
@@ -0,0 +1,18 @@
+package segundoproyramas;
+
+/**
+ *
+ * @author DANIELCASTELAO\mmartinezcosta
+ */
+public class SegundoProyRamas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        System.out.println("A1");
+    }
+    
+}
